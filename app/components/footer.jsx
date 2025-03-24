e13d36645ecd5e8e0e1d94f1252294b8a3c49497
unknown <jesusrodrigo881@gmail.com>
2025-03-23 19:34:41 -0600

    changes

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 export default function Footer() {
     return(
         <footer className="w-full bg-red-elote flex flex-col items-center justify-center mt-10 lg:flex-row">
-                <div className="my-5">
-                <h3 className="text-white font-bold text-2xl mb-4">Redes sociales:</h3>
+            <div className="my-5 lg:order-3 lg:mx-5">
+                <h3 className="text-white font-bold text-2xl mb-4 lg:">Redes sociales:</h3>
                     <div className="flex justify-around items-center">
                         <Image src="/icons/facebook.svg" width={50} height={50} alt="icon facebook" />
                         <Image src="/icons/instagram.svg" width={50} height={50} alt="icon instagram" />
                     </div>
-                </div>
-            <div className="my-5">
+            </div>
+            <div className="my-5 lg:order-2 lg:mx-5">
                 <h3 className="text-white font-bold text-2xl text-center mb-4">Sucursales:</h3>
-                <div className="flex">
+                <div className="flex mb-5">
                     <Image src="/icons/maps.svg" className="mr-2" width={20} height={20} alt="icon maps" />
                     <p className="text-white">Sucursal San Jerónimo</p>
                 </div>
@@ -21,13 +21,13 @@ export default function Footer() {
                     <p className="text-white">Sucursal San Pedro</p>
                 </div>
             </div>
-            <div className="text-center">
+            <div className="text-center lg:order-1 lg:mx-5">
                 <p className="text-white mb-2">Terminos y condiciones.</p>
-                <p className="text-white mb-2">Teléfono: +52 1 8128879985</p>
+                <p className="text-white mb-2">Teléfono: +52 18128879985</p>
                 <p className="text-white mb-2">theeloteco23@gmail.com</p>
-                <p className="text-white mb-10">Todos los derechos reservados por el autor.</p>
+                <p className="text-white mb-10">Todos los derechos<br/> reservados por el autor.</p>
             </div>
-            <div className="w-full bg-[url(/img-mobile/bg-footer.png)] lg:bg-[url(/img-desktop/bg-footer.png)] py-20 footer bg-no-repeat bg-cover flex flex-col items-center justify-center">
+            <div className="w-full bg-[url(/img-mobile/bg-footer.png)] lg:bg-[url(/img-desktop/bg-footer.png)] py-20 footer bg-no-repeat bg-cover flex flex-col items-center justify-center lg:order-4">
             <Image src="/img-mobile/logo-white.png" width={200} height={200} alt="logo The Elote Co" className=""/>
             <p className="mt-2 text-white text-4xl font-bold">The Elote Co.</p>
             </div>
--- app/components/main.jsx
+++ app/components/main.jsx
@@ -6,16 +6,23 @@ export default function Main() {
     return (
         <div className="lg:px-10 md:mx-5">
             <section className="bg-[url(/img-mobile/main-hero.png)] md:bg-[url(/img-desktop/main-hero.png)] w-100 md:w-full h-screen md:h-fit lg:h-screen background-no-repeat bg-no-repeat bg-contain md:bg-contain lg:bg-contain flex flex-col justify-end items-center pb-16 md:justify-center md:items-start lg:pb-0 md:pb-0">
-                <div className="products mb-5 md:order-2">
+                <div className="products mb-5 md:order-2 lg:hidden">
                     <div className="products-slide flex items-center md:ml-10 lg:ml-20">
                         <Image src="/img-mobile/product1.png" alt="background header" width={115} height={155} className="mr-1 md:mx-3"/>
                         <Image src="/img-mobile/product2.png" alt="background header" width={115} height={155} className="mx-1 md:mx-3"/>
                         <Image src="/img-mobile/product3.png" alt= "background header" width={115} height={155} className="ml-1 md:mx-3"/>
                     </div>
                 </div>
+                <div className="products mb-5 md:order-2 max-lg:hidden">
+                    <div className="products-slide flex items-center md:ml-10 lg:ml-20">
+                        <Image src="/img-mobile/product1.png" alt="background header" width={200} height={200} className="mr-1 md:mx-3"/>
+                        <Image src="/img-mobile/product2.png" alt="background header" width={200} height={200} className="mx-1 md:mx-3"/>
+                        <Image src="/img-mobile/product3.png" alt= "background header" width={200} height={200} className="ml-1 md:mx-3"/>
+                    </div>
+                </div>
                 <div className="md:order-1 md:ml-10 lg:ml-20">
                     <h1 className="text-red-700 font-bold text-5xl mb-5 md:text-4xl lg:text-6xl text-center md:text-left lg:mb-10 md:mt-20 lg:mt-0">The Elote Co</h1>
-                    <p className="text-center lg:text-2xl md:w-2/4 md:text-start font-bold text-black-elote px-3 lg:mb-40 md:mb-5">The Elote Co, no te arrepentiras de habernos probado y vas a querer más.</p>
+                    <p className="text-center lg:text-2xl md:w-2/4 md:text-start font-bold text-black-elote px-3 lg:mb-10 md:mb-5">The Elote Co, no te arrepentiras de habernos probado y vas a querer más.</p>
                 </div>
             </section>
             <div className="fixed right-2 top-1/4 max-sm:hidden max-lg:hidden">
